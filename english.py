6bbec5ffb2153e4736615dbbc27afce63eecc16d
Piotr Mardziel <piotrm@gmail.com>
2018-07-18 12:38:18 -0700

    updates

--- english.py
+++ english.py
@@ -6,6 +6,8 @@ experimentally derived perception of strength.
 
 import utils.misc as misc
 
+from typing import List
+
 class Phrase(misc.LocalsMixin):  # pylint: disable=too-few-public-methods
     """ English phrase. """
 
@@ -121,74 +123,74 @@ class VerbPhrase(Modifier):
                             text=self.text + " to")
 
 def _adv(prec: float, strength: float, text: str):
-    return AdverbPhrase(text=text, strength=2.0*(strength-0.5), precision=(100.0-prec)/100.0)
+    return AdverbPhrase(text=text, strength=2.0*(float(strength)/100.-0.5), precision=(100.0-prec)/100.0)
 
 def _adj(prec: float, strength: float, text: str):
-    return AdjectivePhrase(text=text, strength=2.0*(strength-0.5), precision=(100.0-prec)/100.0)
+    return AdjectivePhrase(text=text, strength=2.0*(float(strength)/100.-0.5), precision=(100.0-prec)/100.0)
 
 def _nou(prec: float, strength: float, text: str):
-    return NounPhrase(text=text, strength=2.0*(strength-0.5), precision=(100.0-prec)/100.0)
+    return NounPhrase(text=text, strength=2.0*(float(strength)/100.-0.5), precision=(100.0-prec)/100.0)
 
 def _ver(prec: float, strength: float, text: str):
-    return VerbPhrase(text=text, strength=2.0*(strength-0.5), precision=(100.0-prec)/100.0)
+    return VerbPhrase(text=text, strength=2.0*(float(strength)/100.-0.5), precision=(100.0-prec)/100.0)
 
 # phrases from Quantifying Probabilistic Expressions
 # Author(s): Frederick Mosteller and Cleo Youtz
 # Source: Statistical Science, Vol. 5, No. 1 (Feb., 1990), pp. 2-12
 # https://pdfs.semanticscholar.org/a20c/7c3a5c41d4d7461074da38af95bfa1c78fd0.pdf
 MY_PHRASES = [
-    _adv(0.3, 0.99, "always"),
-    _adj(1.1, 0.97, "certain"),
-    _adv(5.5, 0.91, "almost always"),
-    _nou(5.4, 0.91, "very high probability"),
-    _adv(12.4, 0.87, "very often"),
-    _adj(7.5, 0.86, "almost certain"),
-    _adv(10.1, 0.85, "very likely"), # also adjective
-    _adj(8.9, 0.85, "very probable"),
-    _adj(14.5, 0.81, "very frequent"),
-    _nou(10.1, 0.81, "high probability"),
-    _nou(11.7, 0.81, "high chance"),
-    _adv(16.7, 0.79, "usually"),     # poor precision
-    _adv(15.0, 0.69, "likely"),      # also adjective
-    _adj(13.0, 0.69, "probable"),
-    _adv(10.4, 0.69, "often"),
-    _adj(17.9, 0.66, "liable to happen"),
-    _adj(15.2, 0.61, "frequent"),
-    _adv(3.3, 0.61, "more often than not"),
-    _nou(6.9, 0.58, "better than even chance"),
-    _nou(18.5, 0.52, "moderate probability"),
-    _adv(0.6, 0.50, "as often as not"),
-    _nou(0.5, 0.50, "even chance"),
-    _adj(24.6, 0.45, "not infrequent"),
-    _nou(5.4, 0.41, "less than even chance"),
-    _adj(7.9, 0.38, "less often than not"),
-    _adj(42.7, 0.37, "possible"),
-    _nou(29.1, 0.37, "not unreasonable"),
-    _ver(30.2, 0.36, "might happen"),
-    _adv(17.5, 0.26, "sometimes"),        # poor precision
-    _adv(15.1, 0.23, "now and then"),     # poor precision
-    _adv(15.2, 0.22, "occasionally"),     # poor precision
-    _adv(16.3, 0.19, "unusually"),        # poor precision
-    _adj(12.5, 0.17, "infrequent"),
-    _adv(12.5, 0.17, "once in a while"),
-    _nou(14.5, 0.16, "low probability"),
-    _adv(13.0, 0.16, "unlikely"),        # adjective ?
-    _adj(14.7, 0.15, "improbable"),
-    _adv(14.5, 0.14, "not often"),
-    _adv(14.3, 0.13, "not very often"),
-    _nou(11.3, 0.13, "poor chance"),
-    _nou(7.8, 0.13, "low chance"),
-    _adv(10.1, 0.12, "seldom"),          # also adjective
-    _adv(5.9, 0.08, "very unlikely"),    # adjective ?
-    _adj(6.4, 0.07, "very infrequent"),
-    _adv(6.5, 0.07, "rarely"),
-    _nou(5.7, 0.06, "very low probability"),
-    _adj(5.9, 0.06, "very improbable"),
-    _adv(4.5, 0.06, "very seldom"),
-    _adv(3.8, 0.04, "very rarely"),
-    _adv(3.4, 0.03, "almost never"),
-    _adj(0.3, 0.01, "impossible"),
-    _adv(0.3, 0.01, "never")
+    _adv(0.3, 99, "always"),
+    _adj(1.1, 97, "certain"),
+    _adv(5.5, 91, "almost always"),
+    _nou(5.4, 91, "very high probability"),
+    _adv(12.4, 87, "very often"),
+    _adj(7.5, 86, "almost certain"),
+    _adv(10.1, 85, "very likely"), # also adjective
+    _adj(8.9, 85, "very probable"),
+    _adj(14.5, 81, "very frequent"),
+    _nou(10.1, 81, "high probability"),
+    _nou(11.7, 81, "high chance"),
+    _adv(16.7, 79, "usually"),     # poor precision
+    _adv(15.0, 69, "likely"),      # also adjective
+    _adj(13.0, 69, "probable"),
+    _adv(10.4, 69, "often"),
+    _adj(17.9, 66, "liable to happen"),
+    _adj(15.2, 61, "frequent"),
+    _adv(3.3, 61, "more often than not"),
+    _nou(6.9, 58, "better than even chance"),
+    _nou(18.5, 52, "moderate probability"),
+    _adv(0.6, 50, "as often as not"),
+    _nou(0.5, 50, "even chance"),
+    _adj(24.6, 45, "not infrequent"),
+    _nou(5.4, 41, "less than even chance"),
+    _adj(7.9, 38, "less often than not"),
+    _adj(42.7, 37, "possible"),
+    _nou(29.1, 37, "not unreasonable"),
+    _ver(30.2, 36, "might happen"),
+    _adv(17.5, 26, "sometimes"),        # poor precision
+    _adv(15.1, 23, "now and then"),     # poor precision
+    _adv(15.2, 22, "occasionally"),     # poor precision
+    _adv(16.3, 19, "unusually"),        # poor precision
+    _adj(12.5, 17, "infrequent"),
+    _adv(12.5, 17, "once in a while"),
+    _nou(14.5, 16, "low probability"),
+    _adv(13.0, 16, "unlikely"),        # adjective ?
+    _adj(14.7, 15, "improbable"),
+    _adv(14.5, 14, "not often"),
+    _adv(14.3, 13, "not very often"),
+    _nou(11.3, 13, "poor chance"),
+    _nou(7.8, 13, "low chance"),
+    _adv(10.1, 12, "seldom"),          # also adjective
+    _adv(5.9, 8, "very unlikely"),    # adjective ?
+    _adj(6.4, 7, "very infrequent"),
+    _adv(6.5, 7, "rarely"),
+    _nou(5.7, 6, "very low probability"),
+    _adj(5.9, 6, "very improbable"),
+    _adv(4.5, 6, "very seldom"),
+    _adv(3.8, 4, "very rarely"),
+    _adv(3.4, 3, "almost never"),
+    _adj(0.3, 1, "impossible"),
+    _adv(0.3, 1, "never")
 ]
 
 def closest_modifier(phrases, strength):
@@ -221,3 +223,21 @@ def create_modifier_phrases(min_precision=0.0, resolution=0.1, types=lambda x: T
                                   resolution)]))))
 
     return phrases_best
+
+def oxford_commas(al):
+    ll = len(al)
+    if ll <= 1: return al
+    if ll == 2: return [al[0], " and ", al[1]]
+    ret = []
+    ret.append(al.pop())
+    for _ in range(len(al)-2):
+        ret.append(", ")
+        ret.append(al.pop())
+    ret.append(", and ")
+    ret.append(al.pop())
+
+    return ret
+
+oxford_commas(["1"])
+oxford_commas(["1","2"])
+oxford_commas(["1","2","3"])
